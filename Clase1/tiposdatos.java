6e06ee6910e337347a243c34e3a997e3c34433bb
Ivan Narvaez Mejia <ivan.narvaez@unitecnar.edu.co>
2024-02-22 09:53:38 -0500

    Agregado tipos de datos java y clases en java

---
+++ Clase1/tiposdatos.java
@@ -0,0 +1,27 @@
+//TIPOS DE DATOS ENTEROS
+
+//short su tamaño maximo son 2 bytes
+short dias = (12 * 31) * 5;
+//int su tamaño maximo son 4 bytes
+int velocidadLuz = 299792458;
+//long su tamaño maximo son 8 bytes
+long añoLuz = velocidadLuz * 365;
+
+//TIPOS DE DATOS FLOTANTES / DECIMALES
+
+
+//su tamaño maximo son 4 bytes
+float pi = 3.1415926535f;
+//double su tamaño maximo son 8 bytes
+double e = 2.718281828459045235360;
+
+//TIPO DE DATOS CARACTERES
+
+char letraA = 'a';
+char letraANumerico = 61;
+
+//TIPO DE DATOS BOOLEANOS
+
+boolean verdadero = true;
+boolean falso = false;
+
